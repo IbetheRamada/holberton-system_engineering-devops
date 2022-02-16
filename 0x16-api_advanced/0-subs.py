c4df6d70110e9527e7563a9411c5f0f4f512ceec
IbetheRamada <valerdagd@gmail.com>
2022-02-16 00:07:42 -0500

    add first file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import requests
 """
 Write a function that queries the Reddit API and
 returns the number of subscribers
@@ -8,13 +9,12 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of
     subscribers (not active users, total subscribers)
     """
-    import requests
 
     info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if info.status_code >= 300:
         return 0
 
     return sub_info.json().get("data").get("subscribers")
